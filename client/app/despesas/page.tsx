2c097ae6c5600ee032a48f69003b0048759a4a7f
cmj2001 <charles.js2001@gmail.com>
2025-10-13 21:55:58 -0400

    Resolverx

--- client/app/despesas/page.tsx
+++ client/app/despesas/page.tsx
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 import { Toaster } from "@/components/ui/toaster";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
@@ -6,13 +6,12 @@ import { ExpenseServices } from "@/lib/services/expense.service";
 import ContentInset from "./_components/content-Inset";
 
 export default async function Page() {
-  const apiResult = await ExpenseServices.listPaginate()
-   return (
-     <SidebarProvider>
-       <AppSidebar />
-       <ContentInset data={apiResult.result} />
-       <Toaster />
-     </SidebarProvider>
-   );
+  const apiResult = await ExpenseServices.listPaginate();
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <ContentInset data={apiResult.result} />
+      <Toaster />
+    </SidebarProvider>
+  );
 }
-
